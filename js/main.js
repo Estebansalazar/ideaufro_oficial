0055b38dd774fc45d4685dea722e192c06b8748d
Esteban <esteban.salazar.p@gmail.com>
2019-06-13 11:31:17 -0400

    tercera version

--- js/main.js
+++ js/main.js
@@ -118,6 +118,69 @@
     loop: true,
     items: 1
   });
+  
+  var $config = {
+    animatedText: '.animated-text',
+};
+var templateText = '<div class="{{class}}">{{character}}</div>';
+
+$($config.animatedText).each(function(){
+  var text = $(this).text();
+  $(this).text('');
+
+  for (var key in text){
+    var character = text[key];
+      var templatedCharacter = templateText.replace('{{character}}', text[key]);
+      $(this).append(templatedCharacter);
+  }
+})
 
 })(jQuery);
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = [" iDEAFACTORY"];
+const typingDelay = 80;
+const erasingDelay = 80;
+const newTextDelay = 650; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
 
